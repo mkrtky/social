0f19c966de1a39f3e7fc33eb0b70d75d7c148f86
mkrtky <mkrtky@protonmail.com>
2019-10-21 14:21:05 +0200

    created logout view

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
