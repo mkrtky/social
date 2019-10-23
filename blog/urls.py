6c6bf891a804125cb79edd7a55f3ce94becd4401
mkrtky <mkrtky@protonmail.com>
2019-10-23 15:32:10 +0200

    created post create view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import PostDetailView, PostListView
+from .views import PostDetailView, PostListView, PostCreateView
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post-create')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, ListView, CreateView
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
