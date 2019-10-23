943a7f6558cd04603eb5f0dfd58db7c59c2da6ae
mkrtky <mkrtky@protonmail.com>
2019-10-23 12:50:33 +0200

    created post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import PostDetailView, PostListView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
