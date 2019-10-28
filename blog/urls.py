3ff63a7f5dad4f2073fa503af2e88ced4ef1e72a
mkrtky <mkrtky@protonmail.com>
2019-10-28 10:32:17 +0100

    created post update view

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import PostDetailView, PostListView, PostCreateView, PostDeleteView
+from .views import PostDetailView, PostListView, PostCreateView, PostDeleteView, PostUpdateView
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
